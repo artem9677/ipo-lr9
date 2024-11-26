b30b7a15972fe589799529f21ba3e9710ee672d0
Архипец Артём <ArkhipA_82@students.mgke.loc>
2024-11-26 14:56:33 +0300

    Task 3 complete

---
+++ collision/isCollisionRect.py
@@ -0,0 +1,34 @@
+from .isCorrectRect import isCorrectRect
+
+class RectCorrectError(Exception):
+    pass
+
+def isCollisionRect(list):
+    try:
+        isCorrectRect(list)
+    except RectCorrectError as e:
+        print(e)
+        return False
+    n = len(list)
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            x1, y1 = list[i][0]
+            x2, y2 = list[i][1]
+
+            x3, y3 = list[j][0]
+            x4, y4 = list[j][1]
+            
+            left = max(x1, x3)
+            top = min(y2, y4)
+            right = min(x2, x4)
+            bottom = max(y1, y3)
+
+            width = right - left
+            height = top - bottom
+
+            if width > 0 and height > 0:
+                return True  
+
+    return False  
+    
